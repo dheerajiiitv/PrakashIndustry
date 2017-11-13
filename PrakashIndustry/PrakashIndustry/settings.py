92487a1ac05b3f8c89d9d23273f72aa88aea16d0
Dheeraj Agrawal <dheeraja486@gmail.com>
2017-11-13 21:11:07 +0530

    Added Static url and media

--- PrakashIndustry/PrakashIndustry/settings.py
+++ PrakashIndustry/PrakashIndustry/settings.py
@@ -118,5 +118,9 @@ USE_TZ = True
 LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'ecommerce/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'static_cdn')
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media_cdn')
